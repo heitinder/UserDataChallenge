8fe9729c2ab2f3f5665e9a3edf9c3ca5c1e7e400
heitinder <happymail72@gmail.com>
2014-12-17 15:24:27 -0800

    Initial Commit

---
+++ UserDataChallengeSolution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UserDataChallengeSolution
+//
+//  Created by Heitinder Singh on 12/17/14.
+//  Copyright (c) 2014 Pro-Tek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UserDataChallengeSolution/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UserDataChallengeSolution
+//
+//  Created by Heitinder Singh on 12/17/14.
+//  Copyright (c) 2014 Pro-Tek. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
